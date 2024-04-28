b479e49aea944672f8a2402f34c2a7ebc9bbf6ff
valux <marco.valvano@live.de>
2024-04-28 22:11:48 +0200

    use bash only for debugging

--- start-ollama-mx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
-ollama_mx(){
-    python3 $SCRIPT_DIR/start-ollama-mx.py
-}
-until ollama_mx; do
-    echo "ollama-mx crashed with exit code $?. Restarting..." >&2
-    sleep 1
-done 
